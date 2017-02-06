c7cc3ef8938f6e73de97baeda7302e192d2a26eb
hideo54 <contact@hideo54.com>
2017-02-06 17:36:36 +0900

    Remove debug output

--- reader.py
+++ reader.py
@@ -12,14 +12,12 @@ def connected(tag):
     now_str = str(datetime.datetime.now())
     data_raw = tag.ndef.message[0].data.lstrip('\x02en')
     data = json.loads(data_raw, object_hook=ascii_encode_dict)
-    print data
 
     salt = ''
     with open('salt.txt') as f:
         salt = f.read().rstrip('\n')
     dk = hashlib.pbkdf2_hmac('sha256', data['id'], salt, 114514)
     true_key = binascii.hexlify(dk)
-    print true_key
 
     if data['key'] == true_key:
         print now_str
